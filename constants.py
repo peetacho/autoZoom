f7e63a67582e8b3cc8a4662bb918c349d0d6f209
peetacho <peterchow2001@gmail.com>
2021-01-08 12:29:37 -0500

    added settings

---
+++ constants.py
@@ -0,0 +1,17 @@
+DAYS = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
+
+FILE_NAME = "/Users/peter/Desktop/Programming/Python/autoZoom/schedule.xlsx"
+
+LOGS = "Logs.txt"
+
+CLASS = 0
+ID = 1
+PASSWORD = 2
+DAY = 3
+TIME = 4
+LINK = 5
+
+JOIN_EARLY = 0
+TOGGLE_CRON = 1
+TIME_ZONE = 2
+DEBUG = 3
--- main.py
+++ main.py
@@ -4,45 +4,66 @@ from crontab import CronTab
 import xlrd
 from datetime import date, datetime, timedelta, timezone
 import clipboard
+from constants import *
 
-########################## OPEN LINK #############################
+def delete_logs():
+    f = open(LOGS,"r+")
+    if len(f.readlines()) > 5:
+        f.truncate(0)
+    f.close()
 
-file_name = "/Users/peter/Desktop/Programming/Python/autoZoom/schedule.xlsx"
-schedule = xlrd.open_workbook(file_name)
+delete_logs()
+
+schedule = xlrd.open_workbook(FILE_NAME)
 sheet = schedule.sheet_by_index(0)
 print(" ")
 
+def get_date():
+    x = datetime.today()
+    return x.strftime("['%A, %b %d, %Y', '%H:%M']")
 
 def get_weekday_time() -> list:
     """
     Returns the weekday and time in a list in the format [weekday, time]
-    Weekday is in the list days = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
+    Weekday is in the list DAYS
 
     >>> get_weekday_time()
     ["MON", "12:01pm"]
     >>> get_weekday_time()
     ["WED", "11:53am"]
     """
-    days = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
-    return [days[date.today().weekday()], check_time()]
+    return [DAYS[date.today().weekday()], check_time()]
 
 def check_time():
     """
     Returns current UTC time
     """
     current_utc_time = datetime.now().strftime("%H:%M")
-    # print("Checking Current UTC Time: " + current_utc_time)
     return current_utc_time
 
-def convert_utc_time(time_to_change):
-    """
-    Returns time_to_change in UTC time
-    """
-    converted_time = (datetime.strptime(
-        time_to_change, "%H:%M") - timedelta(hours=8)).strftime("%H:%M")
-    # print("Checking time (converted): " + converted_time)
-    return converted_time
+def parse_setting(s) -> str:
+    return s[s.find("(") + 1:s.find(")")]
 
+def get_settings():
+    zoom_settings = []
+    i = 1
+    while i < 5:
+        setting_str = parse_setting(sheet.cell_value(0, i))
+
+        setting = setting_str
+        try:
+            setting = int(setting_str)
+        except:
+            if setting_str == "True":
+                setting = True
+            elif setting_str == "False":
+                setting = False
+
+        zoom_settings.append(setting)
+        i+=1
+    return zoom_settings
+    
+settings = get_settings()
 
 def get_zoom_details(current_time, current_day):
     """
@@ -52,23 +73,22 @@ def get_zoom_details(current_time, current_day):
 
     zoom_details = [zoom_class, zoom_meeting_id, zoom_password, zoom_day, zoom_time, zoom_link]
 
-    time_index_col = 4
-    day_index_col = 3
-    for row in range(sheet.nrows):
-        zoom_time = sheet.cell_value(row,time_index_col)
-        zoom_day = sheet.cell_value(row,day_index_col)
+    for row in range(1, sheet.nrows):
+        zoom_time = sheet.cell_value(row,TIME)
+        zoom_day = sheet.cell_value(row,DAY)
         if zoom_time == current_time and zoom_day == current_day:
             i = 0
             while i < len(zoom_details):
                 zoom_details[i] = sheet.cell_value(row,i)
                 i+=1
             return zoom_details
+        return None
 
 def set_clipboard():
     """
     Sets clipboard to zoom meeting id.
     """
-    clipboard.copy(zoom_details[2])
+    clipboard.copy(zoom_details[ID])
 
 def send_notification():
     """
@@ -76,7 +96,7 @@ def send_notification():
     """
 
     title = "Sucessfully Joined Zoom Meeting!"
-    message = f'The following zoom meeting password is copied to your clipboard: {zoom_details[2]}'
+    message = f'The following zoom meeting password is copied to your clipboard: {zoom_details[PASSWORD]}'
     command = f'''
     osascript -e 'display notification "{message}" with title "{title}"'
     '''
@@ -98,7 +118,7 @@ def on_class_exists():
     """
     Function is called when there is a class at CURRENT_TIME
     """
-    webbrowser.open(zoom_details[5], new=2)
+    webbrowser.open(zoom_details[LINK], new=2)
     set_clipboard()
     send_notification()
     print("LOG >> Class at current time")
@@ -112,7 +132,8 @@ def on_class_none():
 
 CURRENT_WEEKDAY = get_weekday_time()[0]
 CURRENT_TIME = get_weekday_time()[1]
-print("[CURRENT_WEEKDAY, CURRENT_TIME] >> ", get_weekday_time())
+print("SHORT_DATE >> ", get_weekday_time())
+print("FULL_DATE >> ", get_date())
 
 zoom_details = get_zoom_details(CURRENT_TIME,CURRENT_WEEKDAY)
 print("ZOOM_DETAILS >> ", zoom_details)
@@ -133,32 +154,52 @@ def convert_hour(hour):
 
 
 def check_day(day_index):
-    days = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
-    for day in days:
+    for day in DAYS:
         if day.lower() == day_index.lower():
             return day
     on_fail_notification()
 
+def get_early_time(days,hours,minutes):
+    d = datetime(2020,1,date.today().day,int(hours),int(minutes))
+    td = timedelta(minutes = int(settings[JOIN_EARLY]))
+
+    original_time = d.strftime("%H:%M:%a").split(":")
+    # print(original_time)
+    early_time = (d - td).strftime("%H:%M:%a").split(":")
+    # print(early_time)
+    return early_time
+
 def convert_time_to_tab(time, day_index):
-    minute = time[time.find(":")+1:]
-    hour = convert_hour(time[:time.find(":")])
     day = check_day(day_index)
-    return "{} {} * * {}".format(minute,hour,day)
+    hour = convert_hour(time[:time.find(":")])
+    minute = time[time.find(":")+1:]
 
-cron = CronTab(user='peter')
-cron.remove_all(comment='autoZoom')
+    hour,minute,day = get_early_time(day,hour,minute)
+    return "{} {} * * {}".format(minute,hour,check_day(day))
 
-time_index_col = 4
-day_index_col = 3
-for row in range(1, sheet.nrows):
-    zoom_time = sheet.cell_value(row,time_index_col)
-    zoom_day = sheet.cell_value(row,day_index_col)
-    CRON_TAB = convert_time_to_tab(zoom_time,zoom_day)
+def print_crontab(cron):
+    for line in cron.lines:
+        print(line)
+
+def set_crontab():
+    cron = CronTab(user='peter')
+    cron.remove_all(comment='autoZoom')
+
+    if settings[TOGGLE_CRON]:
+        for row in range(2, sheet.nrows):
+            zoom_time = sheet.cell_value(row,TIME)
+            zoom_day = sheet.cell_value(row,DAY)
+            CRON_TAB = convert_time_to_tab(zoom_time,zoom_day)
+            
+            job = cron.new(command=CRON_COMMAND, comment="autoZoom")
+            job.setall(CRON_TAB)
+
+    cron.write()
     
-    job = cron.new(command=CRON_COMMAND, comment="autoZoom")
-    job.setall(CRON_TAB)
+    if settings[DEBUG]:
+        print(("#" * 35) + " CRONTAB " + ("#" * 35))
+        print_crontab(cron)
 
-cron.write()
 
-# for line in cron.lines:
-#     print(line)
+if __name__ == '__main__':
+    set_crontab()
